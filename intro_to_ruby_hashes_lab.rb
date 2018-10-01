ce6777cef3d7e04e64644eb431be674369529d72
Ana Harris <vana1990@gmail.com>
2018-10-01 03:23:10 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4, 
 	    :names => {
-	      :reading_railroads => {
+	      :reading_railroad => {
 	        
 	      },
 	      :pennsylvania_railroad => {
