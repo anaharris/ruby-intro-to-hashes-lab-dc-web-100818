5e489e4c97f042fa6c544138da79ff1de37d9cfe
Ana Harris <vana1990@gmail.com>
2018-10-01 03:24:35 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,7 +59,7 @@ def monopoly_with_fourth_tier
 	        :mortgage_value => "$100"
 	      },
 	      :pennsylvania_railroad => {
-	        
+	        :mortgage
 	      },
 	      :b_and_o_railroad => {
 	        
